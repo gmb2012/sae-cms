de89e77a3878da1f5c83780e5cd313889ade112d
Bjoern Zapadlo <bjoern.zapadlo@gmail.com>
2014-03-10 20:36:24 +0100

    more content

---
+++ excercises/01_cv_php/cv.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Björn Zapadlo</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <div class="container">
+
+        <div class="page-header row">
+            <div class="col-md-1 hidden-sm hidden-xs big-bullet"></div>
+            <div class="col-md-6">
+                <h1>Björn Zapadlo</h1>
+            </div>
+            <div class="col-md-2">
+                Mainaustr. 90<br />
+                78464 Konstanz<br />
+                +49 172 / 71 88 284<br />
+            </div>
+            <div class="col-md-3">
+                <a href="mailto:bjoern@zapadlo.de"><i class="fa fa-envelope"></i></a>
+                <a href="../icon/twitter"><i class="fa fa-twitter"></i></a>
+                <a href="../icon/twitter"><i class="fa fa-xing"></i></a>
+                <a href="../icon/twitter"><i class="fa fa-linkedin"></i></a>
+                <a href="../icon/twitter"><i class="fa fa-home"></i></a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-11 col-md-offset-1">
+                <nav class="navbar" role="navigation">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Lebenslauf</a></li>
+                        <li><a href="#">Qualifikationen</a></li>
+                        <li><a href="#">Referenzen</a></li>
+                        <li><a href="#">Zeugnisse</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+
+
+        <div class="row">
+            <div class="col-md-6 col-md-offset-1">
+                <h2>Beruflicher Werdegang</h2>
+                <div class="row">
+                    <div class="col-md-3 text-right">seit 2011</div>
+                    <div class="col-md-9">
+                        HolidayCheck AG<br />
+                        Team Manager & Senior Software-Developer<br />
+                        Kerntechnologien: PHP, scala, Java, MongoDB, ElasticSearch, MySQL, Javascript, HTML / CSS
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <h2>Dozententätigkeit</h2>
+
+            </div>
+        </div>
+
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
